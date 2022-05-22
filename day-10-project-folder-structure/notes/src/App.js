1a14bb5899259d50ac95871a96122cbf97604073
koscida <britkos@gmail.com>
2022-05-21 21:46:06 -0400

    day 10 project folder structure notes

---
+++ day-10-project-folder-structure/notes/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react'
+
+
+// named export in arrow function
+// export const App = () => <h1>Welcome to 30 Days Of React</h1>
+
+
+// named export in regular function, function declaration
+// export function App () {
+// 	return <h1>Welcome to 30 Days Of React</h1>
+// }
+
+
+// Recommended for most of the cases
+const App = () => <h1>Welcome to 30 Days Of React</h1>
+export default App
